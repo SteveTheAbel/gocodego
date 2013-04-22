97ee3032f8112996398de443a931563e909b66cd
danieru <danieru.dressler@gmail.com>
2013-04-22 14:48:48 -0600

    Add little bit of cool

---
+++ gocode.go
@@ -0,0 +1,24 @@
+package main
+
+// Welcome to Go! Code!
+// To review our goals are:
+// 1. Learn golang!
+// 2. Do cool things!
+// 3. Impress children!
+//
+// Ready!
+// Set!
+// Go!
+
+import "fmt"
+import "time"
+
+func daniel_dressler() {
+	fmt.Printf("Hello from Calgary!\n")
+	Sleep(3000 * time.Millisecond) // Please decrement as needed!
+}
+
+func main() {
+	daniel_dressler()
+	fmt.Printf("Hope to see you again!\n")
+}
